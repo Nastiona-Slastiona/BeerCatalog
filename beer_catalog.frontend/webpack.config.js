e43c4d7a4d5c531b19b245b80c2ca1b8fbf3eb4c
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2022-02-15 10:26:25 +0300

    Deployed frontend part of the project

--- beer_catalog.frontend/webpack.config.js
+++ beer_catalog.frontend/webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
     mode: 'development',
     entry: ['babel-polyfill', './src/index.jsx'],
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
     },
     devServer: {
