1f709b8d70837c0181544c8e990670b9d1f6740f
Anastasiya Kruglaya <ariaignis.dragon@gmail.com>
2022-02-18 15:06:56 +0300

    Changed constants

--- beer_catalog.frontend/src/constants/serviceUrls.jsx
+++ beer_catalog.frontend/src/constants/serviceUrls.jsx
@@ -1,7 +1,7 @@
 const serviceUrls = {
     getSingleBeer: 'https://api.punkapi.com/v2/beers/:id',
     getLimittedBeers: 'https://api.punkapi.com/v2/beers?page=:currPage&per_page=:page',
-    setFavoriteBeers: 'https://beer-catalog-server.azurewebsites.net/api/setFavoriteBeers'
+    setFavoriteBeers: 'https://beer-catalog-app.azurewebsites.net/api/setFavoriteBeers'
 };
 
 export default serviceUrls;
